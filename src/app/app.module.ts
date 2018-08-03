5a3b7f8cd575f3b20517fd57923757f9e08c5614
Mohamed Amine Saoudi <amininfos@gmail.com>
2018-08-03 16:16:42 +0200

    Ajout de la focntionnalite de routage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,17 +7,29 @@ import { ContentComponent } from './content/content.component';
 import { FooterComponent } from './footer/footer.component';
 
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { DetailCompteComponent } from './detail-compte/detail-compte.component';
+
+const tableRoutage = [
+  {path: 'comptes', component:ContentComponent},
+  {path: 'comptes/:id', component:DetailCompteComponent},
+  {path: '', component:WelcomeComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     ContentComponent,
-    FooterComponent
+    FooterComponent,
+    WelcomeComponent,
+    DetailCompteComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(tableRoutage)
   ],
   providers: [],
   bootstrap: [AppComponent]
